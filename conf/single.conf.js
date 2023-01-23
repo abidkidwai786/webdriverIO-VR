01577ae8ff5cc578021bfac880b9edd290f90001
Abid Kidwai <abidkidwai786@gmail.com>
2023-01-23 18:48:05 +0000

    Updated code for parallel run

--- conf/single.conf.js
+++ conf/single.conf.js
@@ -4,10 +4,6 @@ import {LT_USER, LT_KEY} from '../LT.user.key'
 //import {LT_USERNAME,LT_ACCESS_KEY } from process
 
 
-
-
-
-
 exports.config = {
   
   path: "/wd/hub",  // Patterns to exclude.
@@ -15,36 +11,46 @@ exports.config = {
     // 'path/to/excluded/files'
   ],
 
-  maxInstances: 1,
+  maxInstances: 10,
     
   
   user: LT_USER,  //LambdaTest Username
   key: LT_KEY,  // Access Key
   specs: ["./tests/vr.js"], // vr.js spec file which has the test case
-  buildName: "Webdriver  IO 3",
-  capabilities: [
-    
-    {
-      "LT:Options": {
-        browserName: "Chrome",
-        version: "latest",
-        username:LT_USER,
-        accessKey:LT_KEY,
-        name: "Test WebdriverIO Single",
-        build: "WebDriver VR",
 
-        "smartUI.project": "Wdio-Visual-Test", // Visual regression project name
-        "smartUI.build":"build1",
-      },
+  commonCapabilities: {
+    name: "Parallel Sample Test",
+    build: "Visual Regression Build Parallel 1",
+    "smartUI.project": "Wdio-Visual-a", // Visual regression project name
+    "smartUI.build":"Build 2",
+  },
+
+  capabilities: [
+    {
+      platfrom: "Windows 10",
+      browserName: "Chrome",
+      version: "latest",
+    },
+    {
+      platform: "Windows 10",
+      browserName: "Firefox",
+      version: "latest",
+    },
+    {
+      platform: "Windows 10",
+      browserName: "MicrosoftEdge",
+      version: "latest",
     },
   ],
+
+
  
   logLevel: "info",
   coloredLogs: true,
   screenshotPath: "./errorShots/",
   waitforTimeout: 100000,
   connectionRetryTimeout: 90000,
-  connectionRetryCount: 1,
+  connectionRetryCount: 3,
   path: "/wd/hub",
   hostname: "smartui-hub.lambdatest.com",
   port: 80,
@@ -64,3 +70,8 @@ exports.config = {
 
   reporterSyncInterval: 1000,
 };
+
+exports.config.capabilities.forEach(function (caps) {
+  for (var i in exports.config.commonCapabilities)
+    caps[i] = caps[i] || exports.config.commonCapabilities[i];
+});
--- tests/vr.js
+++ tests/vr.js
@@ -13,7 +13,27 @@ describe("My Health", () => {
     await browser.pause(8000);
     
     console.log("taking screenshot ...")
-    await browser.execute("smartui.takeScreenshot").then(out => { //full page screenshot is not supported on Firefox or Egde
+    await browser.execute("smartui.takeScreenshot=1").then(out => { //full page screenshot is not supported on Firefox or Egde
+      console.log("RESPONSE :", out)
+      return
+    });
+
+    await browser.url("https://www.lambdatest.com/capabilities-generator/");
+
+    await browser.pause(8000);
+    
+    console.log("taking screenshot ...")
+    await browser.execute("smartui.takeScreenshot=2").then(out => { //full page screenshot is not supported on Firefox or Egde
+      console.log("RESPONSE :", out)
+      return
+    });
+
+    await browser.url("https://www.lambdatest.com/support/api-doc/");
+
+    await browser.pause(8000);
+    
+    console.log("taking screenshot ...")
+    await browser.execute("smartui.takeScreenshot=3").then(out => { //full page screenshot is not supported on Firefox or Egde
       console.log("RESPONSE :", out)
       return
     });
